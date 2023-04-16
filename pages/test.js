e9759a0556b4b78cbac3d9b3716ffd17ec725fc6
Calvin-C-M <montolalu28@gmail.com>
2023-04-16 17:31:50 +0700

    Added documentation for SSR routes

---
+++ pages/test.js
@@ -0,0 +1,9 @@
+export default function Test() {
+    return (
+        <>
+        <div>
+            <h1 className="text-3xl text-blue-950">Hello World</h1>
+        </div>
+        </>
+    )
+}
--- server.js
+++ server.js
@@ -5,16 +5,36 @@ const dev = process.env.NODE_ENV !== "production"
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const port = 3000
+const host = 'https://localhost'
+
 app.prepare()
 .then(() => {
     const server = express()
 
+    // Apabila dalam sebuah route dibutuhkan SSR
+    // Konfigurasi dibawah ini
+    // =========================================
+
+    server.get('/', (req, res) => {
+        console.log("Hello")
+        return app.render(req, res, '/index', req.query)
+    })
+
+    server.get('/test', (req,res) => {
+        console.log("Test")
+        return app.render(req, res, '/test', req.query)
+    })
+
+    // =========================================
+
+    // Untuk handle route halaman
     server.get('*', (req,res) => {
         return handle(req,res)
     })
 
-    server.listen(3000, (err) => {
+    server.listen(port, (err) => {
         if(err) throw err
-        
+        console.log(`Server running on ${host + ':' + port}`)
     })
 })
