b5f5e0753485fed5523b0a00c42dcc7950c0dad9
Calvin-C-M <montolalu28@gmail.com>
2023-05-01 22:55:49 +0700

    Change bottom margin of search bar

--- components/Admin/SearchBar.jsx
+++ components/Admin/SearchBar.jsx
@@ -2,15 +2,15 @@ import SearchIcon from '@mui/icons-material/Search';
 
 const SearchBar = ({ setText }) => {
     return (
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between mb-10">
             <h1 className="text-primary">EduTry</h1>
             <div className='relative flex items-center'>
-                <SearchIcon className='absolute' />
+                <SearchIcon className='absolute left-1' />
                 <input 
                     type="text" 
                     onChange={(e) => setText(e.target.value)}
                     placeholder="Search"
-                    className="bg-white rounded-md pl-7 pr-3 py-1 focus:outline-none focus:outline-blue-dark transition-all duration-100"
+                    className="bg-white rounded-md pl-8 pr-3 py-1 focus:outline-none focus:outline-blue-dark transition-all duration-100"
                 />
             </div>
         </div>
