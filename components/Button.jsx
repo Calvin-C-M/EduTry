a8b24d7b7bd6ba60dcaa0a88d6440f48e279d2e7
Calvin-C-M <montolalu28@gmail.com>
2023-05-02 11:58:47 +0700

    Changed button (general) layout

--- components/Button.jsx
+++ components/Button.jsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 const Button = ({ children, href, className }) => {
     if(href) {
         return (
-            <Link href={href} className={"bg-primary font-bold text-white flex items-center justify-center px-8 py-1 w-fit " + className}>
+            <Link href={href} className={className + " bg-primary font-bold text-white flex items-center justify-center px-8 py-1 w-fit"}>
                 {children}
             </Link>
         );        
     }
     return (
-        <button className={"bg-primary font-bold text-white flex items-center justify-center px-8 py-1 w-fit " + className}>
+        <button className={className + " bg-primary font-bold text-white flex items-center justify-center px-8 py-1 w-fit"}>
             {children}
         </button>
     );
