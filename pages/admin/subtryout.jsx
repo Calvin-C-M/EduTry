55990d53e5372140cba5e8bed5907264301eb99b
Calvin-C-M <montolalu28@gmail.com>
2023-05-02 14:37:01 +0700

    Add create button

--- pages/admin/subtryout.jsx
+++ pages/admin/subtryout.jsx
@@ -1,6 +1,8 @@
 import SearchBar from "@/components/Admin/SearchBar";
 import SubTryoutLayout from "@/components/Admin/SubTryoutLayout";
 import SubTryoutCard from "@/components/Admin/SubTryoutCard";
+import Button from "@/components/Button";
+import AddIcon from '@mui/icons-material/Add';
 import { useState } from "react";
 
 const SubTryout = () => {
@@ -11,6 +13,10 @@ const SubTryout = () => {
             <SearchBar setText={setSearchText} />
             <div>
                 <h1 className="mb-3">Sub Tryout - Tryout #3</h1>
+                <Button className="bg-green mb-3">
+                    <AddIcon />
+                    Create
+                </Button>
                 <section className="flex flex-col items-center gap-5 p-5 bg-white">
                     <section>
                         <h2 className="text-center mb-2">TPS</h2>
