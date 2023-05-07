5b6d2b109303f6cd623f50e00080834250973868
amalianurf <amalia19006@mail.unpad.ac.id>
2023-05-07 21:16:21 +0700

    Added question box

--- components/DiscussPanel.jsx
+++ components/DiscussPanel.jsx
@@ -1,21 +1,21 @@
 import React, {useState} from 'react'
 import DiscussCard from './DiscussCard'
 
-const DiscussPanel = ({ data }) => {
+const DiscussPanel = ({ data, handle }) => {
     const [comment, setComment] = useState(false)
 
   return (
-    <div className='w-full h-fit bg-white px-8 py-5 my-8 rounded-xl shadow-md'>
+    <div className='w-full h-fit bg-white px-8 py-5 my-3 rounded-xl shadow-md'>
         <div className='w-full h-fit'>
             <DiscussCard username={data.username} message={data.message} />
         </div>
-        <div className='flex w-full h-fit justify-between my-2'>
-            <div className='font-bold'>
+        <div className='flex-none md:flex w-full h-fit justify-between my-2'>
+            <div className='font-bold text-sm md:text-base my-1 md:my-0'>
                 <p>{data.comments?.length} Komentar</p>
             </div>
-            <div className='flex font-light'>
-                <button className='hover:text-primary hover:font-medium'>Tambahkan Komentar</button>
-                <div onClick={() => setComment(!comment)} className='hover:text-primary hover:font-medium cursor-pointer ml-3'>
+            <div className='flex-none md:flex font-light'>
+                <button onClick={handle} className='hover:text-primary hover:font-medium text-sm md:text-base my-1 md:my-0'>Tambahkan Komentar</button>
+                <div onClick={() => setComment(!comment)} className='hover:text-primary hover:font-medium text-sm md:text-base cursor-pointer my-1 md:my-0 md:ml-3'>
                     {comment ? (<p>Sembunyikan Komentar</p>) : (<p>Tampilkan Komentar</p>)}
                 </div>
             </div>
--- pages/discuss.js
+++ pages/discuss.js
@@ -1,6 +1,8 @@
+import React, {useState} from 'react'
 import Link from 'next/link'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import DiscussPanel from '@/components/DiscussPanel'
+import QuestionBox from '@/components/QuestionBox'
 
 export default function Discuss() {
     const discussData = [
@@ -8,20 +10,42 @@ export default function Discuss() {
         {id:1, username:'Username 2', message:'Lorem ipsum dolor sit amet consectetur. Egestas eu senectus aliquam nunc ornare nulla eu nisl. Amet congue augue amet urna gravida pellentesque lobortis adipiscing. Amet fermentum tristique placerat suscipit. Mattis curabitur faucibus ornare sit lobortis aliquam purus?', comments:[{id:1, username:'Konselor', message:'Lorem ipsum dolor sit amet consectetur. Egestas eu senectus aliquam nunc ornare nulla eu nisl. Eget dignissim libero egestas sit sodales ultricies imperdiet commodo sem. Aliquam ut vestibulum quisque elit mauris quis pretium. Ut porttitor ornare diam morbi at ac. At viverra mauris nisi diam placerat at. Amet congue augue amet urna gravida pellentesque lobortis adipiscing. Amet fermentum tristique placerat suscipit. Mattis curabitur faucibus ornare sit lobortis aliquam purus.'}]},
     ]
 
+    const [modal, setModal] = useState(false)
+    const [comment, setComment] = useState(false)
+
+    const handleModal = () => {
+        if (!comment) {
+            setModal(!modal)
+        }
+    }
+
+    const handleComment = () => {
+        setComment(!comment)
+    }
+
   return (
-    <div className='w-full px-10 md:px-28 py-28'>
-        <div className='flex justify-between'>
-            <div className='flex items-center text-primary'>
-                <Link href='/' className='p-2 mr-3'>
-                    <ArrowBackIosNewIcon />
-                </Link>
-                <h1>Diskusi Tryout #1</h1>
+    <div>
+        <div className='w-full px-10 md:px-28 py-28'>
+            <div className='flex justify-between md:mt-3'>
+                <div className='flex items-center text-primary'>
+                    <Link href='/' className='p-2 mr-3'>
+                        <ArrowBackIosNewIcon />
+                    </Link>
+                    <h1>Diskusi Tryout #1</h1>
+                </div>
+                <button onClick={handleModal} className='hidden md:flex bg-primary font-bold text-white items-center justify-center w-fit h-fit px-3 md:px-5 py-2 text-sm md:text-base hover:brightness-110 transition-all duration-100'>
+                    Tanya Soal
+                </button>
             </div>
-            <button className='bg-primary font-bold text-white flex items-center justify-center w-fit h-fit px-3 md:px-5 py-2 text-sm md:text-base hover:brightness-110 transition-all duration-100'>
-                Tanya Soal
-            </button>
+            <div className='flex md:hidden justify-end mt-3'>
+                <button onClick={handleModal} className='bg-primary font-bold text-white flex items-center justify-center w-fit h-fit px-3 md:px-5 py-2 text-sm md:text-base hover:brightness-110 transition-all duration-100'>
+                    Tanya Soal
+                </button>
+            </div>
+            {discussData.map((data) => <DiscussPanel data={data} handle={handleComment} />)}
         </div>
-        {discussData.map((data) => <DiscussPanel data={data} />)}
+        <QuestionBox send={''} handle={handleModal} modalStatus={modal} name={'Tanya Soal'} placeholder={'Masukkan Pertanyaan'} />
+        <QuestionBox send={''} handle={handleComment} modalStatus={comment} name={'Tambah Komentar'} placeholder={'Masukkan Pertanyaan'} />
     </div>
   )
 }
