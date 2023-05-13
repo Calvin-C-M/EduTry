320c74090b78d16d98c7a071c1060427043ec2f7
Calvin-C-M <montolalu28@gmail.com>
2023-05-13 21:36:51 +0700

    Add create tryout modal

--- pages/admin/tryout.jsx
+++ pages/admin/tryout.jsx
@@ -4,10 +4,14 @@ import { useState, useEffect } from "react";
 import TestImage from "@/public/pict.png"
 import axios from "axios";
 import getUrl from "@/utils/getUrl";
+import Button from "@/components/Button";
+import AddIcon from '@mui/icons-material/Add';
+import TryoutModal from "@/components/Admin/TryoutModal";
 
 const Tryout = () => {
     const [searchText, setSearchText] = useState("")
     const [tryouts, setTryouts] = useState([])
+    const [showFormModal, setShowFormModal] = useState(false)
 
     const url = getUrl()
 
@@ -30,6 +34,13 @@ const Tryout = () => {
             <SearchBar setText={setSearchText} />
             <section>
                 <h1 className="mb-3">Tryout</h1>
+                <Button className="bg-green mb-3" onClick={() => setShowFormModal(!showFormModal)}>
+                    <AddIcon />
+                    Create
+                </Button>
+                {
+                    (showFormModal) ? <TryoutModal setShowModal={() => setShowFormModal(false)}/> : ""
+                }
                 <section className="grid grid-cols-4 gap-3 p-8 bg-white">
                     {
                         tryouts.map(tryout => <TryoutCard key={tryout._id} image={TestImage} title={tryout.nama} date={tryout.created_at} href={`/admin/subtryout/${tryout._id}`} />)
