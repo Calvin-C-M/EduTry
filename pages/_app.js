0e3afe24a50b7ab1220677fbdb63154eb61df829
Calvin-C-M <montolalu28@gmail.com>
2023-05-21 21:34:29 +0700

    Fix _document error

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import '@/styles/globals.css'
 import Layout from '../components/Layout'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>EduTry</title>
+        <link rel="icon" href="./edutry.svg" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
     )
 }
