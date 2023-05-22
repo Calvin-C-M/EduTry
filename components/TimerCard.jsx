edc6bfbcba6d3683a04a0a4d35e8ca7c0819fc9b
Calvin-C-M <montolalu28@gmail.com>
2023-05-22 10:32:50 +0700

    Changed time card text

--- components/TimerCard.jsx
+++ components/TimerCard.jsx
@@ -3,10 +3,12 @@ const TimerCard = ({ timeLeft }) => {
   const minutes = Math.floor((timeLeft % 3600) / 60)
   const seconds = timeLeft % 60
 
+  const setTimeText = (satuan) => (satuan > 10) ? satuan : `0${satuan}`
+
   return (
     <div className='w-28 md:w-full p-2 md:p-5 md:mb-4 bg-white rounded-lg shadow-md'>
         <p className='hidden md:flex w-full font-bold'>Sisa Waktu Pengerjaan</p>
-        <p className='w-full md:w-full font-bold text-lg md:text-3xl flex justify-center md:mt-3'>{hours}:{minutes}:{seconds}</p>
+        <p className='w-full md:w-full font-bold text-lg md:text-3xl flex justify-center md:mt-3'>{setTimeText(hours)}:{setTimeText(minutes)}:{setTimeText(seconds)}</p>
     </div>
   )
 }
