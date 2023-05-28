209ca342b37772c40b939085edf51e9ef6b0cbcf
calvincm <78858431+Calvin-C-M@users.noreply.github.com>
2023-05-28 14:56:34 +0700

    Update NumberCard.jsx for merging Sistem_Pengerjaan_Tryout

--- components/NumberCard.jsx
+++ components/NumberCard.jsx
@@ -9,9 +9,14 @@ const NumberCard = ({ data, select, index, handleClick, button, onClick, href })
                 <button key={i} onClick={() => handleClick(i, select)} className={index == i ? 'w-full py-1 bg-blue-dark border-2 border-blue-dark text-white font-medium text-center rounded-md hover:brightness-110 transition-all duration-100' : data.jawaban != '' ? 'w-full py-1 bg-blue border-2 border-blue text-white font-medium text-center rounded-md hover:brightness-110 transition-all duration-100' : 'w-full py-1 font-medium border-2 border-primary bg-white text-center rounded-md hover:bg-blue-100 transition-all duration-100'}>{i+1}</button>
             )}
         </div>
-        <Link href={href}>
-          <button onClick={onClick} className='flex md:hidden justify-center w-full h-fit bg-primary font-bold text-white text-center p-3 mt-2 rounded-lg'>{button}</button>
-        </Link>
+        <form action="/control/submit">
+          <input 
+            type="hidden" 
+            name="selection" 
+            value={JSON.stringify(data)}
+          />
+          <button type='submit' className='flex md:hidden justify-center w-full h-fit bg-primary font-bold text-white text-center p-3 mt-2 rounded-lg'>{button}</button>
+        </form>
     </div>
   )
 }
