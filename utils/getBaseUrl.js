15430f4ef09853371ca80c63ebbdad33452c16b5
Calvin-C-M <montolalu28@gmail.com>
2023-06-05 01:12:51 +0700

    Fixed getBaseUrl function

--- utils/getBaseUrl.js
+++ utils/getBaseUrl.js
@@ -2,7 +2,7 @@ const getBaseUrl = () => {
     const port = process.env.PORT || 3000
     const hostname = process.env.HOSTNAME || "localhost"
 
-    return `http://${hostname}:${port}`
+    return `https://${hostname}`
 }
  
 export default getBaseUrl;
