575b169108e3cf6e3ff4b8228ca98b7eef9a3d30
Calvin-C-M <montolalu28@gmail.com>
2023-06-04 20:59:22 +0700

    Fix build script

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   useFileSystemPublicRoutes: false,
   images: {
+    unoptimized: true,
     domains: [
       'gopay.co.id', 'upload.wikimedia.org', 'shopeepay.co.id', 'bankmandiri.co.id', 'bri.co.id', 
       'api.imgbb.com', 'i.ibb.co',
